6afe147a36609f3bb8d9f08daf0989587749703c
nimiri <knctrityo@gmail.com>
2023-04-20 22:27:00 +0900

    とりあえず動くところまで

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const urlPrefix = process.env.GITHUB_ACTIONS ? "/example-generate-image-share" : "";
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS && "/example-generate-image-share",
+  publicRuntimeConfig: { urlPrefix },
 }
 
 module.exports = nextConfig
